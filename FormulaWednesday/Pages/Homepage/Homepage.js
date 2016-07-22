f3efcfb582fa134ec3c8943a317cbc3295f50f13
Craig Isenor <craig@educask.com>
2016-07-21 20:57:52 -0700

    British GP update

--- FormulaWednesday/Pages/Homepage/Homepage.js
+++ FormulaWednesday/Pages/Homepage/Homepage.js
@@ -11,9 +11,9 @@ class HomePage extends PageBase {
         return new Promise((resolve, reject) => {
             this.getBlogPosts(5).then((bs) => {
                 this.blogPosts(bs);
-                var esc = FirebaseUtilities.escape("<u>Fernando Alonso</u>: \"Can I stop now?\"<br><u>Pit Wall</u>: \"No Fernando, keep going, we want to see if we can get a point if something happens to other cars.\"<br/><u>Fernando Alonso</u>: \"Awww... ARGH!..\"");
+                var esc = FirebaseUtilities.escape("<u>Nico Rosberg:</u> \"Gearbox problem!\"<br/><u>Pit Wall:</u>\"Driver default 101, chassis default 01.Chassis default 01. Avoid seventh gear, Nico.\"<br/><u>Nico Rosberg:</u>\"What‘s that mean? I have to shift through it.\"<br/><u>Pit Wall:</u>\"Affirm Nico you need to shift through it, affirm.\"");
                 this.quote(FirebaseUtilities.unescape(esc));
-                this.imageSource("http://cdn-1.motorsport.com/static/img/mgl/6000000/6030000/6035000/6035900/6035921/s8/f1-brazilian-gp-2015-fernando-alonso-mclaren-watches-qualifying-from-a-chair-at-the-side-o.jpg");
+                this.imageSource("http://cf.c.ooyala.com/BrdjQ3dDpXVPeQdXcDaZESodQtIjCV54/3Gduepif0T1UGY8H4xMDoxOjBzMTt2bJ");
                 resolve(this);
             }).catch((e) => {
                 this.app.alert(e);
